5702d198903064946ede802307692b2524e0d9f9
persistent-path-1995 <mwpenn94@gmail.com>
2020-11-22 05:49:49 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,4 +3,4 @@ rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 puts "R: #{rainbow[0]}, O: #{rainbow[1]}, Y: #{rainbow[2]}, G: #{rainbow[3]}, B: #{rainbow[4]}, I: #{rainbow[5]}, V: #{_rainbow[6]}"
 end
 
-display_rainbow
+display_rainbow(rainbow)
