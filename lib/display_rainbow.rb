813508d9d81e934da81bab94eed3b9ba9dcc8615
persistent-path-1995 <mwpenn94@gmail.com>
2020-11-22 05:39:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,5 @@ rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'] # Wri
 def display_rainbow
 puts "R: #{display_rainbow[0]}, O: #{display_rainbow[1]}, Y: #{display_rainbow[2]}, G: #{display_rainbow[3]}, B: #{display_rainbow[4]}, I: #{display_rainbow[5]}, V: #{display_rainbow[6]}
 end
-
+end
 display_rainbow
