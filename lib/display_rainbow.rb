7a454a79e4d948f6680efd2b686bf42e1b921c41
persistent-path-1995 <mwpenn94@gmail.com>
2020-11-22 05:46:35 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 def display_rainbow(rainbow) # Write your #display_rainbow method
 puts "R: #{rainbow[0]}, O: #{rainbow[1]}, Y: #{rainbow[2]}, G: #{rainbow[3]}, B: #{rainbow[4]}, I: #{rainbow[5]}, V: #{_rainbow[6]}
 end
